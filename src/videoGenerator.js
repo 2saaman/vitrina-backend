fd8aa1939b43e931ab393702395d322bf62719fa
2saaman <samanoffi00@gmail.com>
2026-09-12 11:48:34 +0500

    YANGILASH

--- src/videoGenerator.js
+++ src/videoGenerator.js
@@ -4,7 +4,7 @@ const { spawn, spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-const SECONDS_PER_IMAGE = 2.5;
+const SECONDS_PER_IMAGE = 3;
 const FPS = 18;
 const WIDTH = 720;
 const HEIGHT = 900;
